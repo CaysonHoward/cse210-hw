aa5ffbe6f1b206a2e0af0e47a3891e11051d4543
caysonhoward <how20041@byui.edu>
2023-01-10 22:25:02 -0700

    Finished Pred2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your letter grade");
+        string TextGrade = Console.ReadLine() ?? "";
+
+        int IntGrade = int.Parse(TextGrade);
+        bool passed = true;
+        string Letter = "";
+
+        if (IntGrade >= 90){
+            Letter = "A";
+        }
+        else if (IntGrade >= 80){
+            Letter = "B";
+        }
+        else if (IntGrade >= 70){
+            Letter = "C";
+        }
+        else if (IntGrade >= 60){
+            Letter = "D";
+            passed = false;
+        }
+        else{
+            Letter = "F";
+            passed = false;
+        }
+
+        Console.WriteLine($"You got an {Letter}");
+
+        if (passed == true){
+            Console.WriteLine("Great job! You passed the class?");
+        }
+        else {
+            Console.WriteLine("Sorry, you did not pass. Try again!");
+        }
     }
 }
